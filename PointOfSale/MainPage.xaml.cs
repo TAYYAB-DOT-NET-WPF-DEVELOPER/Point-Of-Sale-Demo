5941bdb090b74ac5b50cf3e357376d72c4157f5c
TAYYAB-DOT-NET-WPF-DEVELOPER <tayyab.uet.lahore@gmail.com>
2023-09-18 23:45:03 +0500

    commited against POS DEMO

---
+++ PointOfSale/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void DashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            //dashboardUserControl.Visibility = Visibility.Visible;
+            // Hide other UserControls if necessary
+            contentControl.Content = new Dashboard();
+        }
+
+        private void EmployeesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Show Employees UserControl, hide others
+        }
+
+        private void ProductsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    }
